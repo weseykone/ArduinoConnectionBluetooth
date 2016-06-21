ae7cd39bdceca2a41c3da3d2fe06459ba6604b8b
Wesley Koorneef <wesleykoorneef@hotmail.com>
2016-06-21 21:36:44 +0200

    light compatibility
    Added compatibility with a light

--- ArduinoConnection/ArduinoConnection.ino
+++ ArduinoConnection/ArduinoConnection.ino
@@ -1,32 +1,55 @@
 #include <SoftwareSerial.h>   //Software Serial Port
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
 #define RxD 2
 #define TxD 3
-
-int IRSensor = 13;
+#define IRSensor 7
+#define Light 4
 
 SoftwareSerial blueToothSerial(RxD, TxD);
+
+int RECEIVE_PIN = 7;
+IRrecv irreceiver(RECEIVE_PIN);
+decode_results results;
 
 void setup()
 {
   Serial.begin(9600);
   blueToothSerial.begin(9600);
+
   pinMode(RxD, INPUT);
   pinMode(TxD, OUTPUT);
-  pinMode(IRSensor, INPUT)
+  pinMode(IRSensor, INPUT);
+  pinMode(4, OUTPUT);
+  
+  irreceiver.enableIRIn(); // Start the receiver
 }
 
 void loop()
 {
-
   char recvChar;
   while (1)
   {
-    println(digitalRead(IRSensor));
-    
-    if (Serial.available())
-    { //check if there's any data sent from t he local serial terminal, you can add the other applications here
-      recvChar  = Serial.read();
-      blueToothSerial.print(recvChar);
+    if (irreceiver.decode(&results))
+    {
+      if (results.value == 1520217670)
+      {
+        Serial.println(results.value);
+        blueToothSerial.print("You lost bitch#");
+
+        digitalWrite(Light, HIGH);
+        delay(500);
+        digitalWrite(Light, LOW);
+
+        irreceiver.resume();
+      }
     }
+
+    /*if (Serial.available())
+      { //check if there's any data sent from t he local serial terminal, you can add the other applications here
+      }*/
+
+    delay(100); //NEW LINE
   }
 }
